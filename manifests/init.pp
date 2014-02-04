78a593d2e732c8e049caf350d6bcb5cb16caacd2
Mike Lehner <mlehner616@gmail.com>
2014-02-04 17:37:57 -0500

    Added clauses to prevent this from running on non-supported OSes.

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,9 +12,24 @@
 #
 class bash-history {
 
-  file{'/etc/profile.d/history.sh':
-    ensure  => file,
-    mode    => '0644',
-    source  => 'puppet:///modules/bash-history/history.sh',
+  case $::osfamily {
+    'RedHat': {
+      case $::operatingsystemmajrelease {
+        '6': {
+          file{'/etc/profile.d/history.sh':
+            ensure  => file,
+            mode    => '0644',
+            source  => 'puppet:///modules/bash-history/history.sh',
+          }
+        }
+        default: {
+          fail("operatingsystemmajrelease is <${::operatingsystemmajrelease}> and this module supports RedHat version 6.")
+        }
+      }
+    }
+    default: {
+      fail("osfamily is <${::osfamily}> and this module supports RedHat.")
+    }
   }
+
 }
